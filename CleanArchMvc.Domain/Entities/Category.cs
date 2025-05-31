3bc3b5ed8b2ebd753d32a4bd7641e567b367a818
black-adm <mattheuscontato17@gmail.com>
2025-05-31 00:02:14 -0300

    fix: enriching and validating product domain model and updating entities to use base class

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -3,8 +3,11 @@ using CleanArchMvc.Domain.Validation;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public sealed class Category
+    public sealed class Category : Entity
     {
+        public string Name { get; private set; }
+        public ICollection<Product> Products { get; private set; }
+
         public Category(string name)
         {
             ValidateDomain(name);
@@ -17,21 +20,18 @@ namespace CleanArchMvc.Domain.Entities
             ValidateDomain(name);
         }
 
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-        public ICollection<Product> Products { get; private set; }
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
 
         private void ValidateDomain(string name)
         {
-            DomainExceptionValidation.When(
-                string.IsNullOrEmpty(name),
-                "Name cannot be null or empty."
-            );
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Name cannot be null or empty.");
 
-            DomainExceptionValidation.When(
-                name.Length < 3,
-                "Invalid name, too short, minimum 3 characters."
-            );
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters.");
 
             Name = name;
         }
