7334e0c78f32cde2fc2227c623bd0c29b1a57a6e
black-adm <mattheuscontato17@gmail.com>
2025-05-31 00:18:57 -0300

    fix: domain entity repository interfaces

--- CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -1,7 +1,15 @@
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+
 namespace CleanArchMvc.Domain.Interfaces
 {
     public interface ICategoryRepository
     {
-        
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(int? id);
+        Task<Category> CreateCategoryAsync(Category category);
+        Task<Category> UpdateCategoryAsync(Category category);
+        Task<Category> RemoveCategoryAsync(Category category);
     }
 }
--- CleanArchMvc.Domain/Interfaces/IProductRepository.cs
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+
 namespace CleanArchMvc.Domain.Interfaces
 {
     public interface IProductRepository
     {
-        
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? categoryId);
+        Task<Product> CreateProductAsync(Product category);
+        Task<Product> UpdateProductAsync(Product category);
+        Task<Product> RemoveProductAsync(Product category);
     }
 }
